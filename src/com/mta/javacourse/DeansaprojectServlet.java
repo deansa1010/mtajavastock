0c1a1715b93708da5cf9f909372787a53b8c764f
deansassi <deansassi1010@gmail.com>
2014-11-09 23:27:57 +0200

    Calculation # 2

--- src/com/mta/javacourse/DeansaprojectServlet.java
+++ src/com/mta/javacourse/DeansaprojectServlet.java
@@ -5,7 +5,21 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class DeansaprojectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, Dean Sassi and good luck");
+		resp.setContentType("text/html");
+		int num1;
+		int num2;
+		int num3;
+		
+		num1 = 4;
+		num2 = 3;
+		num3 = 7;
+		
+		int result = (num1 + num2)*num3;
+		String resultstr = new String("<h1>result of("+num1+"+"+num2+")*"+num3+"="+result+"</h1>");
+		resp.getWriter().println(resultstr);
+		
+			
+	
+		
 	}
 }
