fef51de90aa05719bff3bef655036c28a75eb0c7
deansassi <deansassi1010@gmail.com>
2014-11-17 18:43:52 +0200

    exercise 3

--- src/com/mta/javacourse/DeansaprojectServlet.java
+++ src/com/mta/javacourse/DeansaprojectServlet.java
@@ -6,17 +6,32 @@ import javax.servlet.http.*;
 public class DeansaprojectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/html");
-		int num1;
-		int num2;
-		int num3;
 		
-		num1 = 4;
-		num2 = 3;
-		num3 = 7;
+		int radius = 50;
+		double area = radius * radius * Math.PI;
 		
-		int result = (num1 + num2)*num3;
-		String resultstr = new String("<h1>result of("+num1+"+"+num2+")*"+num3+"="+result+"</h1>");
-		resp.getWriter().println(resultstr);
+		String line1 = new String("calculation 1: Area of circle with radius "+radius+" is "+area+" squere cm");
+		
+		double angleB;
+		double hypotenuse = 50;
+		double opposite;
+	    angleB = 30 * (	Math.PI/180);
+	    opposite = hypotenuse * Math.sin(angleB);
+	    
+	    String line2 = new String("calculation 2: Length of opposite where angle b is 30 degrees and hypotenuse is "+hypotenuse+" cm is "+opposite+"cm");
+	    
+	    double base = 20;
+	    double exp = 13;
+	    double res = 1;
+	    
+	    res = Math.pow(base, exp);
+	    
+	    
+	    String line3 = new String("calculation 3: Power of "+base+" with exp of "+exp+" is "+res+"");
+	    
+	    String resultStr = line1 + "<br>" + line2 + "<br>" +line3;
+	    		
+		resp.getWriter().println(resultStr);
 		
 			
 	
